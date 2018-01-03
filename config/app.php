8118d47bd522b5c490129d65da3821df3914eddc
syamimh <syamimhazmi@gmail.com>
2018-01-04 02:51:37 +0800

    Install and configure laratrust on project

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third Party Service Provider
+         */
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Third Party Aliases
+         */
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
